e52d5af59d76113847f2732131593e5920090571
Sama-Elkhateeb <sama.elkhateeb@guc.edu.eg>
2026-08-19 22:26:37 +0300

    added application status functionality.

--- src/main/java/com/banquemisr/recruitment/data/enums/ApplicationStatus.java
+++ src/main/java/com/banquemisr/recruitment/data/enums/ApplicationStatus.java
@@ -1,11 +1,55 @@
 package com.banquemisr.recruitment.data.enums;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public enum ApplicationStatus {
-    APPLIED,
-    SCREENING,
-    INTERVIEW,
-    OFFER,
-    HIRED,
-    REJECTED,
-    WITHDRAWN
+    APPLIED {
+        @Override
+        public Set<ApplicationStatus> nextStates(){
+        return EnumSet.of(SCREENING, REJECTED, WITHDRAWN);
+        }
+    },
+    SCREENING{
+        @Override
+        public Set<ApplicationStatus> nextStates(){
+            return EnumSet.of(INTERVIEW,REJECTED,WITHDRAWN);
+        }
+    },
+    INTERVIEW{
+        @Override
+        public Set<ApplicationStatus> nextStates(){
+            return EnumSet.of(OFFER, REJECTED, WITHDRAWN);
+        }
+    },
+    OFFER{
+        @Override
+        public Set<ApplicationStatus> nextStates(){
+            return EnumSet.of(HIRED, REJECTED, WITHDRAWN);
+        }
+    },
+    HIRED{
+        @Override
+        public Set<ApplicationStatus> nextStates(){
+            return EnumSet.noneOf(ApplicationStatus.class); // no more states after that
+        }
+    },
+    REJECTED{
+        @Override
+        public Set<ApplicationStatus> nextStates(){
+            return EnumSet.noneOf(ApplicationStatus.class);
+        }
+    },
+    WITHDRAWN{
+        @Override
+        public Set<ApplicationStatus> nextStates(){
+            return EnumSet.noneOf(ApplicationStatus.class);
+        }
+    };
+
+    public abstract Set<ApplicationStatus> nextStates();
+
+    public boolean canTransitionTo(ApplicationStatus target){
+        return nextStates().contains(target);
+    }
 }
